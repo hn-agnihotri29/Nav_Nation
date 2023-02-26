839c1cf7f10f9eafa514fdaa27ba8d525eaacae4
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2023-02-26 11:59:56 +0530

    env updation

--- main.js
+++ main.js
@@ -27,6 +27,6 @@ app.get("/path", function(req, res) {
     res.send("Hello")
 })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server is running on port 3000")
 })
